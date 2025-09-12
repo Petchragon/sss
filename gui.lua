c463f1c47422ed67ee597e251805a37b04472f43
Petchragon <ratthapoom1152@hotmail.com>
2025-09-12 11:00:22 +0700

    ลอง

--- gui.lua
+++
@@ -1,8 +0,0 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Petch", "DarkTheme")
-
-local Tab = Window:NewTab("Main")
-local Section = Tab:NewSection("Claim compass")
-Section:NewToggle("Claim", "ToggleInfo", function(state)
-
-end)
