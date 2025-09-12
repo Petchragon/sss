5c5d8f2fd6a7cb63bb3bbb374632712ba44fd553
Petchragon <ratthapoom1152@hotmail.com>
2025-09-12 10:35:48 +0700

    ลอง

---
+++ claim.lua
@@ -0,0 +1,18 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+-- ตั้งค่า Remote และ Args
+local remote = game:GetService("ReplicatedStorage").Connections:WaitForChild("Claim_Sam")
+local args = { "Claim1" }
+
+local autoClaim = true -- เปิดทำงานอัตโนมัติทันที
+
+-- ฟังก์ชัน Auto Claim Loop
+task.spawn(function()
+    while true do
+        task.wait(3)
+        if autoClaim then
+            remote:FireServer(unpack(args))
+        end
+    end
+end)
