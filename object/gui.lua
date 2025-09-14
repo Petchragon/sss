e2439061d07ee71efe428b6e608b1735b99300a3
Petchragon <ratthapoom1152@hotmail.com>
2025-09-14 22:42:24 +0700

    Update gui.lua

--- object/gui.lua
+++ object/gui.lua
@@ -301,7 +301,7 @@ function Kavo.CreateLib(kavName, themeList)
     MinimizeButton.Size = UDim2.new(0, 21, 0, 21)
     MinimizeButton.ZIndex = 2
     MinimizeButton.Image = "rbxassetid://3926305904"
-    MinimizeButton.ImageRectOffset = Vector2.new(290, 150)
+    MinimizeButton.ImageRectOffset = Vector2.new(290, 240)
     MinimizeButton.ImageRectSize = Vector2.new(24, 24)
     MinimizeButton.MouseButton1Click:Connect(function()
         if not isMinimized then
