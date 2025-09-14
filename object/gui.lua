d8ba5b1efa07a3942747a00234c38b32399e4889
Petchragon <ratthapoom1152@hotmail.com>
2025-09-14 18:27:17 +0700

    test

--- object/gui.lua
+++ object/gui.lua
@@ -311,7 +311,7 @@ function Kavo.CreateLib(kavName, themeList)
         }):Play()
         wait()
         game.TweenService:Create(Main, TweenInfo.new(0.1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {
-			Size = UDim2.new(0,510,0,), 
+			Size = UDim2.new(0,510,0,45), 
 			Position = UDim2.new(0, Main.AbsolutePosition.X + (Main.AbsoluteSize.X / 2), 0, Main.AbsolutePosition.Y + (Main.AbsoluteSize.Y / 2))
 		}):Play()
         wait(1)
