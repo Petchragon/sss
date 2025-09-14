e7dc46f757eded4181123afa4d4719f0b2b10989
Petchragon <ratthapoom1152@hotmail.com>
2025-09-14 19:58:11 +0700

    test

--- object/gui.lua
+++ object/gui.lua
@@ -298,11 +298,11 @@ function Kavo.CreateLib(kavName, themeList)
     MinimizeButton.Parent = MainHeader
     MinimizeButton.BackgroundTransparency = 1.000
     MinimizeButton.Position = UDim2.new(0.949999988, -50, 0.137999997, 0)
-    MinimizeButton.Size = UDim2.new(0, 13, 0, 13)
+    MinimizeButton.Size = UDim2.new(0, 21, 0, 21)
     MinimizeButton.ZIndex = 2
     MinimizeButton.Image = "https://icons.veryicon.com/png/o/miscellaneous/skent-icon/minimize-8.png"
     MinimizeButton.ImageRectOffset = Vector2.new(284, 4)
-    MinimizeButton.ImageRectSize = Vector2.new(13, 13)
+    MinimizeButton.ImageRectSize = Vector2.new(24, 24)
     MinimizeButton.ScaleType = Enum.ScaleType.Crop
     MinimizeButton.MouseButton1Click:Connect(function()
         if not isMinimized then
