42b2853b99042c97688240d071731369df3021eb
Petchragon <ratthapoom1152@hotmail.com>
2025-09-14 18:32:53 +0700

    test

--- object/gui.lua
+++ object/gui.lua
@@ -298,11 +298,12 @@ function Kavo.CreateLib(kavName, themeList)
     MinimizeButton.Parent = MainHeader
     MinimizeButton.BackgroundTransparency = 1.000
     MinimizeButton.Position = UDim2.new(0.949999988, -50, 0.137999997, 0)
-    MinimizeButton.Size = UDim2.new(0, 21, 0, 21)
+    MinimizeButton.Size = UDim2.new(0, 13, 0, 13)
     MinimizeButton.ZIndex = 2
     MinimizeButton.Image = "http://www.roblox.com/asset/?id=5597105827"
     MinimizeButton.ImageRectOffset = Vector2.new(284, 4)
     MinimizeButton.ImageRectSize = Vector2.new(13, 13)
+    MinimizeButton.ScaleType = Enum.ScaleType.Crop
     MinimizeButton.MouseButton1Click:Connect(function()
         if not isMinimized then
         game.TweenService:Create(MinimizeButton, TweenInfo.new(0.1, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {
@@ -310,7 +311,7 @@ function Kavo.CreateLib(kavName, themeList)
         }):Play()
         wait()
         game.TweenService:Create(Main, TweenInfo.new(0.1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {
-			Size = UDim2.new(0,510,0,40),
+			Size = UDim2.new(0,525,0,40),
 			Position = UDim2.new(0, Main.AbsolutePosition.X + (Main.AbsoluteSize.X / 2), 0, Main.AbsolutePosition.Y + (Main.AbsoluteSize.Y / 2))
 		}):Play()
         wait(1)
