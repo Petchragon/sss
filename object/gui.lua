cd430821b1310affada97312f067ee306271f7a0
Petchragon <ratthapoom1152@hotmail.com>
2025-09-14 18:17:31 +0700

    test

--- object/gui.lua
+++ object/gui.lua
@@ -303,6 +303,7 @@ function Kavo.CreateLib(kavName, themeList)
     MinimizeButton.Image = "rbxassetid://5597105827"
     MinimizeButton.ImageRectOffset = Vector2.new(284, 4)
     MinimizeButton.ImageRectSize = Vector2.new(24, 24)
+    MinimizeButton.ScaleType = Enum.ScaleType.Fit
     MinimizeButton.MouseButton1Click:Connect(function()
         if not isMinimized then
         game.TweenService:Create(MinimizeButton, TweenInfo.new(0.1, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {
@@ -310,7 +311,7 @@ function Kavo.CreateLib(kavName, themeList)
         }):Play()
         wait()
         game.TweenService:Create(Main, TweenInfo.new(0.1, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {
-			Size = UDim2.new(0,520,0,40), 
+			Size = UDim2.new(0,510,0,45), 
 			Position = UDim2.new(0, Main.AbsolutePosition.X + (Main.AbsoluteSize.X / 2), 0, Main.AbsolutePosition.Y + (Main.AbsoluteSize.Y / 2))
 		}):Play()
         wait(1)
