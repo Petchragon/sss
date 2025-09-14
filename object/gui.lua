e96c178b113b452d0cd67622f0c9b9767111fec0
Petchragon <ratthapoom1152@hotmail.com>
2025-09-15 01:34:03 +0700

    Update gui.lua

--- object/gui.lua
+++ object/gui.lua
@@ -175,7 +175,7 @@ function lib:Window(text)
 	minimize.Parent = top
 	minimize.BackgroundTransparency = 1.000
 	minimize.LayoutOrder = 6
-	minimize.Position = UDim2.new(0.931818187, -30, 0.111111112, 0)
+	minimize.Position = UDim2.new(0.931818187, -25, 0.111111112, 0)
 	minimize.Size = UDim2.new(0, 20, 0, 20)
 	minimize.ZIndex = 2
 	minimize.Image = "rbxassetid://3926307971"
