483469c46215e530ec570ac160869043cac55a75
Petchragon <ratthapoom1152@hotmail.com>
2025-09-14 23:31:25 +0700

    V1.5

--- one piece mythical.lua
+++ one piece mythical.lua
@@ -1,5 +1,5 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Petch", "DarkTheme")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Petchragon/sss/refs/heads/Claim/compass/object/gui.lua"))()
+local Window = Library.CreateLib("Petchragon", "Midnight")
 
 local Tab = Window:NewTab("Main")
 
